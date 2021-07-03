f52b0a38ae0f3aa28fd3b1c85495ab30c3d54ac4
nightf0rc3 <katercolin@gmail.com>
2021-07-03 15:06:46 +0200

    changed to portal2 using puppeteer

--- main.js
+++ main.js
@@ -1,63 +1,65 @@
-const Browser = require('zombie');
+const puppeteer = require('puppeteer');
 const telegramBot = require('node-telegram-bot-api');
 const config = require('./config.json');
+const _ = require('lodash');
 
 const bot = new telegramBot(config.botToken, { polling: true });
 
 const gradeMap = [];
 let initialRun = true;
 
-bot.on('message', (msg) => {
-    console.log('[+] Message from ' + msg.chat.id);
-    bot.sendMessage(msg.chat.id, 'hi');
-});
+async function navigateByClick(page, selector) {
+  return Promise.all([
+    page.waitForNavigation(),
+    page.click(selector)
+  ]);
+}
 
-function checkAndNotify() {
-    console.log('[*] Checking for new grades...');
-    const browser = new Browser();
-    browser.visit(config.loginUrl, () => {
-        browser.fill('username', config.user);
-        browser.fill('password', config.password);
-        browser.pressButton('Login', () => {
-            browser.clickLink('.divlinks a:nth-child(4)', () => {
-                browser.clickLink('#makronavigation ul li:nth-child(3) a', () => {
-                    const table = browser.querySelectorAll('table tbody')[7].innerHTML;
-                    const rows = table.split('<tr bgcolor="#EFEFEF">');
-                    rows.forEach(row => {
-                        const cols = row.split('<td ');
-                        if (cols.length > 1) {
-                            const examName = cols[5].replace('class="posrecords">', '').replace('</td>', '').trim();
-                            const examGrade = cols[8].replace('align="center" class="posrecords">', '').replace('</td>', '').replace('&nbsp;', '').trim();
-                            const examTry = cols[12].replace('align="center" class="posrecords">', '').replace('</td>', '').replace('&nbsp;', '').trim();
-                            if (gradeMap[examName+examTry] === undefined) {
-                                gradeMap[examName+examTry] = examGrade;
-                                console.log('[+] New Grade found: ' + examGrade + ' in ' + examName);
-                                const infoString = `New Grade in _${examName}_: *${examGrade}*`;
-                                if (!initialRun) {
-                                    notifyUser(infoString);
-                                }
-                            }
-                        }
-                    });
-                    browser.tabs.closeAll()
-                });
-            });
-        });
-    });
+async function checkAndNotify() {
+  console.log('[*] Checking for new grades...');
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto(config.loginUrl);
+  await page.type('#username', config.user);
+  await page.type('#password', config.password);
+  await navigateByClick(page, 'input[name=submit]');
+  console.log('[*] Logged in');
+  await page.goto(config.examUrl);
+  await navigateByClick(page, '.liste li:nth-child(3) a');
+  await navigateByClick(page, 'li.treelist a:nth-child(3)');
+  const rows = await page.evaluate(`
+    $(".tabelle1, .tabelle1_alignright").map(function() {
+      return this.innerText;
+    }).get()
+  `);
+  const examResults = _.chunk(rows, 6);
+  examResults.forEach((ex) => {
+    const examName = ex[0];
+    const examGrade = ex[3];
+    const examTry = ex[5];
+    if (gradeMap[examName+examTry] === undefined) {
+      gradeMap[examName+examTry] = examGrade;
+      console.log('[+] New Grade found: ' + examGrade + ' in ' + examName);
+      const infoString = `New Grade in _${examName}_: *${examGrade}*`;
+      if (!initialRun) {
+          notifyUser(infoString);
+      }
+    }
+  });
 }
 
 function checkTime() {
-    initialRun = false;
-    const d = new Date();
-    if (d.getHours() > config.activityStart && d.getHours() < config.activityEnd) {
-        checkAndNotify();
-    }
+  initialRun = false;
+  const d = new Date();
+  if (d.getHours() > config.activityStart && d.getHours() < config.activityEnd) {
+      checkAndNotify();
+  }
 }
 
 function notifyUser(message) {
-    bot.sendMessage(config.telegramUserId, message, {
-        parse_mode: 'Markdown'
-    });
+  bot.sendMessage(config.telegramUserId, message, {
+      parse_mode: 'Markdown'
+  });
 }
 
 setInterval(checkTime, 1000 * 60 * config.interval);
